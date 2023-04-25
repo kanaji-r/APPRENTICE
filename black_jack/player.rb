03d5c55e7357f06c1635861639ca1c4d0315b32e
kanaji-r <qypght561012@gmail.com>
2023-04-26 01:22:29 +0900

    delete comment

--- black_jack/player.rb
+++ black_jack/player.rb
@@ -12,20 +12,4 @@ class Player
   def draw(deck, number)
     @cards += deck.draw(number)
   end
-
-  # def select_draw(number)
-  #   while calculate_total(player_cards) < 21
-  #     # カードを引くか選択
-  #     puts "あなたの得点は#{calculate_total(player_cards)}点です。カードを引きますか？（Y/N）"
-  #     answer = gets.chomp.upcase
-  #     if answer == "Y"
-  #       # トランプからカードを一枚取る(自分)
-  #       player_cards += draw_cards(deck, number)
-  #       p "あなたの引いたカードは#{player_cards[-1][:suit]}の#{player_cards[-1][:rank]}です。"
-
-  #     else
-  #       break
-  #     end
-  #   end
-  # end
 end
